492abc9b479cd6ac30baa223f245df67d67a762c
GodfreyTimba <GodfreyT@qode.healthcare>
2019-11-14 09:21:13 +0200

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    'url' : 'mongodb+srv://stockvel:a8PYCSPtwqS8LH0o@mongodev-fol53.mongodb.net/stokvel_database?retryWrites=true&w=majority' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+     
+     //Please replace your host file Here : 127.1.1.0 , Express is Collection Name (Database Name)
+};
